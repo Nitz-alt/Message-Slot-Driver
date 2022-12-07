00115ff6c1901c883c7600917b3ca2ddefde2ad7
Nitzan Yizhar <nitzanyizhar@localhost.localdomain>
2022-12-07 19:28:17 +0200

    fixed some warning. Need to change the structer of the channels table

--- message_slot.c
+++ message_slot.c
@@ -10,7 +10,7 @@
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>
 #include <linux/slab.h>
-
+#include <linux/errno.h>
 
 
 
@@ -45,9 +45,11 @@ static int device_open(struct inode *inode, struct file *fp){
     int minor = iminor(inode);
     if (DEVICES[minor] == NULL){
         /* Starting size will be 10 and we will allocate more if we need more */
-        DEVICES[minor] = kmalloc(sizeof(struct channel) * 10, GFP_KERNEL);
+        DEVICES[minor] = (struct channel *) kmalloc(sizeof(struct channel) * 10, GFP_KERNEL);
+        if (DEVICES[minor] == NULL) return -1;
         memset(DEVICES[minor], 0, sizeof(struct channel) * 10);
-        BOUNDRIES[minor] = kmalloc(sizeof(struct arrayBoundries), GFP_KERNEL);
+        BOUNDRIES[minor] = (struct arrayBoundries *) kmalloc(sizeof(struct arrayBoundries), GFP_KERNEL);
+        if(BOUNDRIES[minor] == NULL) return -1;
         BOUNDRIES[minor] -> aSize = 10;
         BOUNDRIES[minor] -> iNum = 0;
     }
@@ -60,8 +62,7 @@ static int device_open(struct inode *inode, struct file *fp){
 static int device_ioctl(struct file *fp, unsigned int channelId, unsigned long command){
     int minor;
     if (command != MSG_SLOT_CHANNEL || channelId == 0){
-        errno = EINVAL;
-        return -1;
+        return -EINVAL;
     }
     minor = (int) fp->private_data;
     /* Checking if the channel even exists will happen in the write/read functions */
@@ -70,23 +71,21 @@ static int device_ioctl(struct file *fp, unsigned int channelId, unsigned long c
 }
 
 static ssize_t device_read(struct file *fp, char* userbuffer, size_t length, loff_t *offset){
-    int minor, channelId, bytesRead = 0, i;
+    int minor, channelId, i;
     struct arrayBoundries *chBound;
     struct channel *channel_array, *ch;
-    minor = fp->private_data;;
     char *msg;
+    minor = (int) fp->private_data;
     if (minor == -1){
-        errno = EINVAL;
-        return -1;
+        return -EINVAL;
     }
     channelId = USER_CHANNELS[minor];
     if (channelId == -1){
         /* Not channel has been set*/
-        errno = EINVAL;
-        return -1;
+        return -EINVAL;
     }
     chBound = BOUNDRIES[minor];
-    channels_array = DEVICES[minor];
+    channel_array = DEVICES[minor];
     /* char *msg = DEVICES[minor][channel].MESSAGE; */
     /* We need to search for the channel strcuture with the specific id*/
     for (i = 0; i < chBound->iNum; i++){
@@ -95,13 +94,11 @@ static ssize_t device_read(struct file *fp, char* userbuffer, size_t length, lof
     }
     if (i == chBound->iNum){
         /* Channel wasn't found */
-        errno = EWOULDBLOCK;
-        return -1;
+        return -EWOULDBLOCK;
     }
     if (length < ch->messageSize){
         /* The buffer size is smaller than the message */
-        errno = ENOSPC;
-        return -1;
+        return -ENOSPC;
     }
     msg = ch->MESSAGE;
     for (i = 0; i < length && i < MESSAGE_LEN; i++){
@@ -115,40 +112,40 @@ static ssize_t device_write(struct file *fp, const char *userBuffer, ssize_t len
     struct arrayBoundries *bounds;
     struct channel *ch, *channel_Array;
     char *msg;
-    ssize_t i;
+    ssize_t j;
     if (length > MESSAGE_LEN){
-        errno = EMSGSIZE;
-        return -1;
+        return -EMSGSIZE;
     }
-    minor = fp -> private_data;
+    minor = (int) fp -> private_data;
     channelId = USER_CHANNELS[minor];
     if (channelId == -1){
         /* No channel has been set */
-        errno = EINVAL;
-        return -1;
+        return -EINVAL;
     }
     bounds = BOUNDRIES[minor];
     /* We need to look for the channel in the table */
     channel_Array = DEVICES[minor];
-    for (i = 0; i < bounds.iNum; i++){
+    for (i = 0; i < bounds->iNum; i++){
         ch = channel_Array + i;
         if (ch->id == channelId) break;
     }
-    if (i == bounds.iNum){
+    if (i == bounds->iNum){
         /* There is no channel with the requested id ==> We need to create one */
         /* No space in the array we need to realloc (by a factor of 2)*/
-        if (bounds.iNum == bounds.aSize){
-            DEVICES[minor] = krealloc(DEVICES[minor], bounds->aSize * 2 * sizeof(struct channel), GFP_KERNEL);
+        if (bounds->iNum == bounds->aSize){
+            DEVICES[minor] = (struct channel * ) krealloc(DEVICES[minor], bounds->aSize * 2 * sizeof(struct channel), GFP_KERNEL);
+            if (DEVICES[minor] == NULL) return -1;
             bounds -> aSize = bounds->aSize * 2;
         }
-        DEVICES[minor][bounds.iNum] = kmalloc(sizeof(struct channel), GFP_KERNEL);
-        ch = DEVICES[minor][bounds.iNum];
+        DEVICES[minor][bounds->iNum] = (struct channel) kmalloc(sizeof(struct channel), GFP_KERNEL);
+        if (DEVICES[minor][bounds->iNum] == NULL) return -1;
+        ch = &(DEVICES[minor][bounds->iNum]);
         bounds->iNum++;
     }
     /* After channel lookup or creation we need to copy the data */
     msg = ch->MESSAGE;
-    for (i = 0; i < length; i++){
-        get_user(*msg, *userBuffer);
+    for (j = 0; j < length; j++){
+        get_user(*msg, userBuffer);
         msg++;
         userBuffer++;
     }
@@ -156,13 +153,13 @@ static ssize_t device_write(struct file *fp, const char *userBuffer, ssize_t len
     return i;
 }
 
-struct file_operations{
-    .owner = THIS_MODULE;
-    .read = device_read;
-    .write = device_write;
-    .open = device_open;
-    .ioctl = device_ioctl;
-}Fops;
+struct file_operations Fops = {
+    .owner = THIS_MODULE,
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .unlocked_ioctl = device_ioctl,
+};
 
 
 static int __init init_message_slot(void){
