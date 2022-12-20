1929c83a75387149b640bd0b8642ced804451fe1
Nitzan Yizhar <nitzanyizhar@gmail.com>
2022-12-20 17:07:55 +0200

    Changed returns on errors to exitting

--- message_reader.c
+++ message_reader.c
@@ -14,33 +14,32 @@ int main(int argc, char *argv[]){
     int fd, channelId, bytesRead;
     if (argc != 3){
         perror("Nubmber of arguments insufficient.\nPlease provide 2 arguments. Device path and channel id");
-        return -1;
+        exit(1);
     }
    /* Enough argumnets given */
     fd = open(argv[1], O_RDONLY);
     if (fd < 0){
         perror("Error opening file");
-        return -1;
+        exit(1);
     }
     /* File opened correctly */
-    /* TODO: How do we check if its acutally a device */
     /* Parsing channel id */
     channelId = atoi(argv[2]);
     if (ioctl(fd, MSG_SLOT_CHANNEL, channelId) < 0){
         perror("Error setting channel id");
-        return -1;
+        exit(1);
     }
     if ((bytesRead = read(fd, msg, MESSAGE_LEN)) < 0){
         perror("Error reading from device");
-        return -1;
+        exit(1);
     }
     if (close(fd) < 0){
         perror("Error closing file");
-        return -1;
+        exit(1);
     }
     if (write(STDOUT_FILENO, msg, bytesRead) < 0){
         perror("Error printing message");
-        return -1;
+        exit(1);
     }
     return SUCCESS;
 }
--- message_sender.c
+++ message_sender.c
@@ -14,31 +14,30 @@ int main(int argc, char *argv[]){
     int fd, channelId, messageLen;
     if (argc != 4){
         perror("Nubmber of arguments insufficient.\nPlease provide 3 arguments. Device path and channel id and message");
-        return -1;
+        exit(1);
     }
    /* Enough argumnets given */
     fd = open(argv[1], O_WRONLY);
     if (fd < 0){
         perror("Error opening file");
-        return -1;
+        exit(1);
     }
     /* File opened correctly */
-    /* TODO: How do we check if its acutally a device */
     /* Parsing channel id */
     channelId = atoi(argv[2]);
     if (ioctl(fd, MSG_SLOT_CHANNEL, channelId) < 0){
         perror("Error setting channel id");
-        return -1;
+        exit(1);
     }
     /* Fiding length of the message given */
     messageLen = strlen(argv[3]);
     if (write(fd, argv[3], messageLen) < 0){
         perror("Error writing to device");
-        return -1;
+        exit(1);
     }
     if (close(fd) < 0){
         perror("Error closing file");
-        return -1;
+        exit(1);
     }
     return SUCCESS;
 }
